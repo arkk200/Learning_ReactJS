4c9be9e03c1168a8de42960dbbee2ae1b7409057
arkk200 <arkk20000@gmail.com>
2022-07-31 23:25:32 +0900

    Manage value in components through of useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import Hello2 from './Hello2';
-import Wrapper from './Wrapper';
-import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (      
-    <Wrapper>
-      <Hello2 name='첫번째 자식 컴포넌트' color='hotpink' isSpecial={true}
-      // true는 Javascript 값이기에 중괄호를 써준다.
-      /> 
-      <Hello2 name='두번째 자식 컴포넌트' color='#6B360F' />
-      <Hello2 name='세번째 자식 컴포넌트' color='#6B360F' isSpecial
-      // props의 이름만 설정하고 값을 생략한다면 이를 true로 간주한다.
-      />
-    </Wrapper>
+    <div>
+      <Counter />
+    </div>
   );
 }
 
--- src/Hello3.js
+++
@@ -1,7 +0,0 @@
-// ES6에 새로 나온 비구조화 할당 (구조 분해)를 이용하여 코드를 좀 더 간결하게 만들 수 있다.
-function Hello3({color, name}){
-    // props로 전달 받은 값을 이용하여 스타일도 정의 해줄 수 있다.
-    return <div style={{color:color}}>{name}</div>
-}
-
-export default Hello3;

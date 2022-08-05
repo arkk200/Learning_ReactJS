a32165313111f4a8dc601d2c838236bb06a290df
arkk200 <arkk20000@gmail.com>
2022-08-06 06:21:03 +0900

    21. Create Custom Hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useRef, useReducer, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import useInputs from './hooks/useInputs';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
@@ -8,10 +9,6 @@ function countActiveUsers(users) {
 }
 
 const initialState = {
-  inputs: {
-    username: '',
-    email: ''
-  },
   users: [
     {
       id: 1,
@@ -36,29 +33,18 @@ const initialState = {
 
 function reducer(state, action){
   switch (action.type){
-    case 'CHANGE_INPUT':
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value
-        }
-      };
     case 'CREATE_USER':
       return {
-        inputs: initialState.inputs,
         users: state.users.concat(action.user)
       };
     case 'TOGGLE_USER':
       return {
-        ...state,
         users: state.users.map(user => 
           user.id === action.id ? {...user, active: !user.active} : user
         )
       };
     case 'REMOVE_USER':
       return {
-        ...state,
         users: state.users.filter(user => user.id !== action.id)
       };
     default:
@@ -66,22 +52,20 @@ function reducer(state, action){
   }
 }
 
+// Context API를 쓰면 특정 함수를 여러개의 컴포넌트를 거치지 않고 전달할 수 있다.
+// Context API는 React.createContext()로 만들 수 있고 인자로 초깃값을 정할 수 있다.
+export const UserDispatch = React.createContext(null);
+
 function App() {
+  const [{ username, email }, onChange, reset] = useInputs({
+    username: '',
+    email: ''
+  });
   // useState, useReduce 중 뭘 써야하는가에 대한 답은 정해져 있지 않다. 상황에 따라 편리하다고 느끼는 것을 쓰면 된다.
   const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
 
   const {users} = state;
-  const {username, email} = state.inputs;
-
-  const onChange = useCallback(event => {
-    const {name, value} = event.target
-    dispatch({
-      type:'CHANGE_INPUT',
-      name,
-      value
-    });
-  }, []);
 
   const onCreate = useCallback(() => {
     dispatch({
@@ -92,8 +76,9 @@ function App() {
         email
       }
     });
+    reset();
     nextId.current += 1;
-  }, [username, email]);
+  }, [username, email, reset]);
 
   const onToggle = useCallback(id => {
     dispatch({
@@ -111,11 +96,11 @@ function App() {
 
   const count = useMemo(() => countActiveUsers(users), [users]);
   return (
-    <>
+    <UserDispatch.Provider value={dispatch}>
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
       <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
       <div>활성사용자 수 : {count}</div>
-    </>
+    </UserDispatch.Provider>
   );
 }
 

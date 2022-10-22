a0c69b8ad595b6f59a92c5e91b347e588bcded4d
arkk200 <arkk20000@gmail.com>
2022-10-23 03:52:17 +0900

    23. Immer 를 사용한 더 쉬운 불변성 관리

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Counter from './Counter';
 
 // ReactDOM은 브라우저에 리액트 컴포넌트를 렌더링해준다.
 // 여기서 id가 root인 DOM을 선택하고 있는데 이 DOM은
